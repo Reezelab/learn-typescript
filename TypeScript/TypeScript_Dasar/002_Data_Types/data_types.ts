f7d4b3868c3c4216f4cfb5eddc2dffe03fba5f10
neibreen <azfaalharits25@gmail.com>
2024-04-18 21:37:07 +0700

    first commit

---
+++ TypeScript/TypeScript_Dasar/002_Data_Types/data_types.ts
@@ -0,0 +1,24 @@
+// tipe data di typescript mirip sekali dengan tipe data di javascript diantaranya yaitu...
+
+
+// string
+console.log(typeof 'Hello World!');
+
+// number
+console.log(typeof 100);
+console.log(typeof 3.14);
+
+// boolean
+console.log(typeof true);
+console.log(typeof false);
+
+// array
+console.log(typeof [10, 20, 30, 40, 50]);
+
+// object
+console.log(typeof {nama: 'John Doe', address: 'United States'})
+
+// null, NaN, undefine
+console.log(typeof null)
+console.log(typeof NaN)
+console.log(typeof undefined)
---
+++ TypeScript/TypeScript_Dasar/003_Variable/variable.ts
@@ -0,0 +1,4 @@
+// sama seperti di javascript, ada tiga jenis variable di typescript yaitu var, let, const
+// di typescript kita direkomendasikan mendefinisikan variable dengan tipe data yang akan di tampungnya (static type)
+// namun kita tetap bisa menggnakan variable tanpa mendefinisikan tipe datanya dan nantinya otomatis definisi variablenya mengikuti nilai yang ditampunya
+// atau bisa juga dengan mendefinisikan any yang artinya dapat menampung tipe data apa saja (dynamic type)
