b5a02e6431b335dad1f64d2d49656b25fd9b64ef
neibreen <azfaalharits25@gmail.com>
2024-04-21 12:17:30 +0700

    folders

--- TypeScript/TypeScript_Dasar/002_Data_Types/data_types.ts
+++ TypeScript/TypeScript_Dasar/002_Data_Types/data_types.ts
@@ -1,6 +1,8 @@
 // tipe data di typescript mirip sekali dengan tipe data di javascript diantaranya yaitu...
 
 
+// ---tipe data primitif (tipe data yang sederhana) ---
+
 // string
 console.log(typeof 'Hello World!');
 
@@ -12,13 +14,19 @@ console.log(typeof 3.14);
 console.log(typeof true);
 console.log(typeof false);
 
+// null, NaN, undefine
+console.log(typeof null)
+console.log(typeof NaN)
+console.log(typeof undefined)
+
+
+
+// tipe data non-primitif (tipe data yang kompleks)
+
 // array
 console.log(typeof [10, 20, 30, 40, 50]);
 
 // object
 console.log(typeof {nama: 'John Doe', address: 'United States'})
 
-// null, NaN, undefine
-console.log(typeof null)
-console.log(typeof NaN)
-console.log(typeof undefined)
+// function, enum, dan interface termasuk tipe data non-primitif
